6a55a982f9475f69a20e4f9b5261661d3f1e674c
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-01 17:17:50 +0000

    github.sh skeleton created; used by github.go

--- internal/github/github.go
+++ internal/github/github.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	_ "embed"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -11,28 +12,55 @@ import (
 
 var log = logit.Logit().WithLevel(logit.LevelInfo)
 
+//go:embed static/github.sh
+var githubSh string
+
 type GH struct {
+	Metadata *Metadata
 }
 
 func newGH() (*GH, error) {
-	return &GH{}, nil
+	metadata, err := NewMetadata()
+	if err != nil {
+		return nil, err
+	}
+	log.Info("metadata loaded", "metadata", metadata)
+	return &GH{Metadata: metadata}, nil
 }
 
-func (g *GH) run() error {
-	log.Info("GH.run()")
-	m, err := NewMetadata()
-	if err != nil {
-		return err
-	}
-	log.Info("metadata loaded", "m", m)
-	cmd := exec.Command("gh", "release", "view", m.Tag)
+func (g *GH) releaseView() error {
+	cmd := exec.Command("gh", "release", "view", g.Metadata.Tag)
 	bo, be, err := cmdutil.RunSimple(cmd)
 	if err != nil {
-		return fmt.Errorf("'gh release view %s' failed': %s (stdout=%s, stderr=%s)", m.Tag, err, bo.String(), be.String())
+		return fmt.Errorf("'gh release view %s' failed': %s (stdout=%s, stderr=%s)", g.Metadata.Tag, err, bo.String(), be.String())
 	}
 	lines := strings.SplitSeq(strings.TrimSpace(bo.String()), "\n")
 	for line := range lines {
 		log.Info("gh-release-view>", "line", line)
+	}
+	return nil
+}
+
+func (g *GH) githubSh() error {
+	cmd := exec.Command("bash", "-c", githubSh)
+	bo, be, err := cmdutil.RunSimple(cmd)
+	if err != nil {
+		return fmt.Errorf("github script execution failed: %s (stdout=%s, stderr=%s)", err, bo.String(), be.String())
+	}
+	lines := strings.SplitSeq(strings.TrimSpace(bo.String()), "\n")
+	for line := range lines {
+		log.Info("github-sh>", "line", line)
+	}
+	return nil
+}
+
+func (g *GH) run() error {
+	log.Info("GH.run()")
+	if err := g.releaseView(); err != nil {
+		return err
+	}
+	if err := g.githubSh(); err != nil {
+		return err
 	}
 	return nil
 }
