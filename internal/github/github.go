19ea2453b2c3c70d2e681c4b163eceb3cce69bfa
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-01 18:04:54 +0000

    github.sh: 'dist/rootfs.raw' generated with binary and extension-release details

--- internal/github/github.go
+++ internal/github/github.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	"bytes"
 	_ "embed"
 	"fmt"
 	"os/exec"
@@ -13,7 +14,7 @@ import (
 var log = logit.Logit().WithLevel(logit.LevelInfo)
 
 //go:embed static/github.sh
-var githubSh string
+var githubSh []byte
 
 type GH struct {
 	Metadata *Metadata
@@ -28,21 +29,9 @@ func newGH() (*GH, error) {
 	return &GH{Metadata: metadata}, nil
 }
 
-func (g *GH) releaseView() error {
-	cmd := exec.Command("gh", "release", "view", g.Metadata.Tag)
-	bo, be, err := cmdutil.RunSimple(cmd)
-	if err != nil {
-		return fmt.Errorf("'gh release view %s' failed': %s (stdout=%s, stderr=%s)", g.Metadata.Tag, err, bo.String(), be.String())
-	}
-	lines := strings.SplitSeq(strings.TrimSpace(bo.String()), "\n")
-	for line := range lines {
-		log.Info("gh-release-view>", "line", line)
-	}
-	return nil
-}
-
 func (g *GH) githubSh() error {
-	cmd := exec.Command("bash", "-c", githubSh)
+	cmd := exec.Command("bash", "-s", g.Metadata.ProjectName)
+	cmd.Stdin = bytes.NewBuffer(githubSh)
 	bo, be, err := cmdutil.RunSimple(cmd)
 	if err != nil {
 		return fmt.Errorf("github script execution failed: %s (stdout=%s, stderr=%s)", err, bo.String(), be.String())
@@ -55,14 +44,7 @@ func (g *GH) githubSh() error {
 }
 
 func (g *GH) run() error {
-	log.Info("GH.run()")
-	if err := g.releaseView(); err != nil {
-		return err
-	}
-	if err := g.githubSh(); err != nil {
-		return err
-	}
-	return nil
+	return g.githubSh()
 }
 
 func Run() error {
--- internal/github/static/github.sh
+++ internal/github/static/github.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-for i in $(seq 1 10); do
-  echo $i
-done
+PROJECT_NAME="$1"
+set -x -e -o pipefail
+mkdir -p dist/rootfs/usr/bin dist/rootfs/usr/lib/extension-release.d dist/rootfs/usr/lib64
+cp "dist/${PROJECT_NAME}_linux_amd64_v1/${PROJECT_NAME}" "dist/rootfs/usr/bin/${PROJECT_NAME}"
+cat <<__EOF >"dist/rootfs/usr/lib/extension-release.d/extension-release.${PROJECT_NAME}-flatcar"
+ID=flatcar
+SYSEXT_LEVEL=1.0
+ARCHITECTURE=x86-64
+__EOF
+#cp /usr/bin/tmux /usr/bin/rclone dist/rootfs/usr/bin
+#bash -c "cp -a /usr/lib/x86_64-linux-gnu/libutempter.so* dist/rootfs/usr/lib64"
+mksquashfs dist/rootfs dist/rootfs.raw -noappend -comp zstd -all-root
