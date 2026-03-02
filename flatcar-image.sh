56585307cc530ca732efc48d47f971cd60f2b7e8
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-02 16:09:02 +0000

    flatcar-image.sh: extension uploaded to release

--- flatcar-image.sh
+++ flatcar-image.sh
@@ -21,21 +21,26 @@ eval "$(jq -r '. | "export PROJECT_NAME=\"\(.project_name)\"\nexport TAG=\"\(.ta
 echo "PROJECT_NAME ... '${PROJECT_NAME}'"
 echo "TAG ............ '${TAG}'"
 
+FLATCAR_EXT="${PROJECT_NAME}-flatcar"
+FLATCAR_RAW="${DIST}/${FLATCAR_EXT}.raw"
+
 set -x
 
 rm -rf "${ROOTFS}"
 mkdir -p "${ROOTFS}/usr/bin" "${ROOTFS}/usr/lib/extension-release.d" "${ROOTFS}/usr/lib64"
 cp "${DIST}/${PROJECT_NAME}_linux_amd64_v1/${PROJECT_NAME}" "${ROOTFS}/usr/bin/${PROJECT_NAME}"
-cat <<__EOF >"${ROOTFS}/usr/lib/extension-release.d/extension-release.${PROJECT_NAME}-flatcar"
+cat <<__EOF >"${ROOTFS}/usr/lib/extension-release.d/extension-release.${FLATCAR_EXT}"
 ID=flatcar
 SYSEXT_LEVEL=1.0
 ARCHITECTURE=x86-64
 __EOF
 cp /usr/bin/tmux "${ROOTFS}/usr/bin/tmux"
 cp -a /usr/lib/x86_64-linux-gnu/libutempter.so* "${ROOTFS}/usr/lib64"
-mksquashfs "${ROOTFS}" "${ROOTFS}.raw" -noappend -comp zstd -all-root
-#find dist -type f | sort | gzip -9 >"${DIST}/dist.txt.gz"
-#find /usr/ -type f | sort | gzip -9 >"${DIST}/usr.txt.gz"
-#dpkg -l | gzip -9 >"${DIST}/dpkg.txt.gz"
-# [ "$GITHUB_TOKEN" = "" ] && exit 0 # don't upload in local environment
-# gh release upload "$TAG" "${DIST}/dist.txt.gz" "${DIST}/usr.txt.gz" "${DIST}/dpkg.txt.gz"
+mksquashfs "${ROOTFS}" "${FLATCAR_RAW}" -noappend -comp zstd -all-root
+[ "$GITHUB_TOKEN" = "" ] && exit 0 # don't upload in local environment
+gh release upload "${TAG}" "${FLATCAR_RAW}"
+
+# find dist -type f | sort | gzip -9 >"${DIST}/dist.txt.gz"
+# find /usr/ -type f | sort | gzip -9 >"${DIST}/usr.txt.gz"
+# dpkg -l | gzip -9 >"${DIST}/dpkg.txt.gz"
+# gh release upload "${TAG}" "${DIST}/dist.txt.gz" "${DIST}/usr.txt.gz" "${DIST}/dpkg.txt.gz"
