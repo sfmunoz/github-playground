4dce80e4edc55beb65da11682d1712f44b2aefc1
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-02 18:07:58 +0000

    go code dropped: vanilla 'flatcar-image.sh' will be used

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sfmunoz/github-playground
 
 go 1.25.0
-
-require github.com/sfmunoz/logit v1.0.0
--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"github.com/sfmunoz/github-playground/internal/github"
-	"github.com/sfmunoz/logit"
-)
-
-var log = logit.Logit().WithLevel(logit.LevelInfo)
-
 func main() {
-	log.Info("github-playground")
-	if err := github.Run(); err != nil {
-		log.Fatal("github.Run() failed", "err", err)
-	}
+	println("github-playground")
 }
